d0d67e719d875796b3cd5fa336db12a84ff82888
Naphaz <664230050@webmail.npru.ac.th>
2025-11-19 15:33:26 +0700

    4

--- Home.py
+++ Home.py
@@ -23,10 +23,20 @@ with col3:
    st.image("./img/iris3.jpg") 
 
 html_7 = """
-<div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<div style="background-color:##FF6347;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>สถิติข้อมูลดอกไม้</h5></center>
 </div>
 """
 st.markdown(html_7, unsafe_allow_html=True)
 st.markdown("")
 
+dt = pd.read_csv("./data/iris.csv")
+st.write(dt.head(10))
+
+dt1 = dt['petallength'].sum()
+dt2 = dt['petalwidth'].sum()
+dt3 = dt['sepallength'].sum()
+dt4 = dt['sepalwidth'].sum()
+
+dx = [dt1, dt2, dt3, dt4]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
